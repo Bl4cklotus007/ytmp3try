25807bf2e964704394e46e6c7bfc1083f0b604e1
Bl4cklotus007 <155674870+Bl4cklotus007@users.noreply.github.com>
2025-06-14 21:21:11 +0700

    Initial commit: YouTube to MP3 converter with auto-rename feature

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add your project directory to the sys.path
-path = '/home/YOUR_USERNAME/ytdownloadermp3'
+path = '/home/Bl4cklotus007/ytdownloadermp3'
 if path not in sys.path:
     sys.path.append(path)
 
